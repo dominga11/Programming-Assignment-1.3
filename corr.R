c48ac3f2adc63cec5d242ca2ec1c21064eab2ac4
Amber DeNeal <dominga11@users.noreply.github.com>
2014-08-09 23:04:39 -0400

    Create corr.R

---
+++ corr.R
@@ -0,0 +1,13 @@
+corr <- function(directory, threshold=0) {
+  directory=c(getwd())
+  CompleteData<-numeric()
+  for(i in list.files(directory)){
+    filepaths=read.csv(paste(directory,"/",sprintf("%s",i),".csv",sep =""))
+    CompleteData<-nrow(filepaths[complete.cases(filepaths),])
+    if(CompleteData>threshold){
+      CompleteData<-c(CompleteData,cor(filepaths$nitrate,filepaths$sulfate,use="complete.obs"))
+    }
+    as.numeric(CompleteData)
+  }
+  return(cr)
+}
